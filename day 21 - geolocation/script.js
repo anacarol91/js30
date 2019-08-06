af222cd918a6f67f9e82b792aeb1b525d44cb239
Ana Carolina <anacarolina@somar.io>
2019-08-06 19:40:50 -0300

    days 21 and 22 completed

---
+++ day 21 - geolocation/script.js
@@ -0,0 +1,12 @@
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  data => {
+    speed.textContent = data.coords.speed;
+    arrow.getElementsByClassName.transform = `rotate(${
+      data.coords.heading
+    }deg)`;
+  },
+  err => alert("Turn on the location")
+);
